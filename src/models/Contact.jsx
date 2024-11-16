02fc9ad6ff3861873637c2e06a38fd1144d85ce6
unknown <2012laela@gmail.com>
2024-11-16 14:52:31 +0800

    navbar-closed

--- src/models/Contact.jsx
+++ src/models/Contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Button from '../layouts/Button';
 
 const Contact = ({ closeForm }) => {
     return (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
             <div className='popup-form absolute mt-12 text-black'>
                 <form className='w-80 md:w-96 space-y-5 p-5 rounded-xl bg-white'>
-                    <h1 className='text-4xl font-semibold text-center'>Book Now!</h1>
+                    <h1 className='text-4xl font-semibold text-center text-teal-300'>Book Now!</h1>
                     <div className='flex flex-col'>
-                        <input type="text" name='userFirstName' id='userFirstName' placeholder='First Name' className='py-3 px-2 bg-[#d5f2ec] rounded-lg' />
+                        <input type="text" name='userFirstName' id='userFirstName' placeholder='First Name' className='py-3 px-2 bg-[#d5f2ec] rounded-lg border-black' />
                     </div>
                     <div className='flex flex-col'>
                         <input type="text" name='userLastname' id='userLastname' placeholder='Last Name' className='py-3 px-2 bg-[#d5f2ec] rounded-lg' />
@@ -19,8 +20,8 @@ const Contact = ({ closeForm }) => {
                         <input type="number" name='userNumber' id='userNumber' placeholder='Phone No.' className='py-3 px-2 bg-[#d5f2ec] rounded-lg' />
                     </div>
 
-                    <div>
-                        <button className='bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300 transition duration-300 ease-in-out'>Book Appointment!</button>
+                    <div className='flex gap-5'>
+                        <Button title="Book Appointment">Book Appointment!</Button>
                         <button className='bg-indigo-400 text-white px-10 rounded-md active:bg-red-500' onClick={closeForm}>Close</button>
                     </div>
                 </form>
