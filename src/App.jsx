111f75f6ba51fc89e53d42c606e9b01ba97818bb
unknown <2012laela@gmail.com>
2024-11-16 17:20:51 +0800

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    
+    <main>
+      <div id='home'>
+        <Home />
+      </div>
+    </main>
     </>
   )
 }
