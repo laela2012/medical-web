acd40773d9c6799c45be70d24e8ec13d74b18dc4
unknown <2012laela@gmail.com>
2024-11-16 23:23:41 +0800

    footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     return (
             <div className='fixed w-full z-10 text-white'>
                 <div>
-                    <div className='flex flex-row justify-between p-5 md:px-32 px-5 bg-indigo-400 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
+                    <div className='flex flex-row justify-between p-5 md:px-32 px-5 bg-backgroundColor shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
                         <div className='flex flex-row items-center cursor-pointer'>
                             <Link to='home' spy={true} smooth={true} duration={500} >
                                 <h1 className='text-2xl font-semibold'>Wellness Vista.</h1>
@@ -37,11 +37,11 @@ const Navbar = () => {
                         </div>
 
                         <nav className='hidden lg:flex flex-row items-center text-lg font-medium gap-8'>
-                            <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer'>Home</Link>
-                            <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer'>About Us</Link>
-                            <Link to='services' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer'>Services</Link>
-                            <Link to='doctors' spy={true} smooth={true} duration={500} className='hover:text-orange-300 cursor-pointer'>Doctors</Link>
-                            <Link to='blog' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer'>Blog</Link>
+                            <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Home</Link>
+                            <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>About Us</Link>
+                            <Link to='services' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Services</Link>
+                            <Link to='doctors' spy={true} smooth={true} duration={500} className='hover:text-hoverColor cursor-pointer'>Doctors</Link>
+                            <Link to='blog' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Blog</Link>
                         </nav>
                         <div className='hidden lg:flex'>
                             <button className='bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300 transition duration-300 ease-in-out' onClick={openForm}>Contact Us</button>
@@ -57,12 +57,12 @@ const Navbar = () => {
                             )}
                         </div>
                     </div>
-                    <div className={`${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-indigo-400 text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
-                        <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer' onClick={closeMenu}>Home</Link>
-                        <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer' onClick={closeMenu}>About Us</Link>
-                        <Link to='services' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer' onClick={closeMenu}>Services</Link>
-                        <Link to='doctors' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer' onClick={closeMenu}>Doctors</Link>
-                        <Link to='blog' spy={true} smooth={true} duration={500} className='hover:text-orange-300 transition-all cursor-pointer' onClick={closeMenu}>Blog</Link>
+                    <div className={`${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-backgroundColor text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+                        <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer' onClick={closeMenu}>Home</Link>
+                        <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer' onClick={closeMenu}>About Us</Link>
+                        <Link to='services' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer' onClick={closeMenu}>Services</Link>
+                        <Link to='doctors' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer' onClick={closeMenu}>Doctors</Link>
+                        <Link to='blog' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer' onClick={closeMenu}>Blog</Link>
 
                         <div className='lg:hidden'>
                             <button className='bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300 transition duration-300 ease-in-out' onClick={openForm}>Contact Us</button>
